6692ec6d817ec892e2e25793b03ab3e82083f749
Jack <jack_waayer@hotmail.co.nz>
2017-04-05 00:31:24 +1200

    Learing graph theory

--- softwareTesting2017/Program.cs
+++ softwareTesting2017/Program.cs
@@ -10,24 +10,47 @@ namespace softwareTesting2017
     {
         static void Main(string[] args)
         {
+            int seed = Int16.Parse(args[0]);
             Simulation mySim = new Simulation();
         }
     }
 
     class Simulation
     {
-        private string[] locations = new string[5] {"Mayfair", "Akina", "Stortford Lodge", "Mahora", "Outside City"};
-        private string[] exits = new string[4] { "Karamu Road", "Havelock Road", "Railway Road", "Omahu Road" };
-        private string[] streets;
+        private string[] locations = new string[] {"Mayfair", "Akina", "Stortford Lodge", "Mahora", "Outside City"};
+        private string[] exits = new string[] { "Karamu Road", "Havelock Road", "Railway Road", "Omahu Road" };
         private int[] drivers = new int[5] { 1, 2, 3, 4, 5 };
         private int akinaCount = 0;
 
-        int randomNumberGenerator(int seed)
+        /* 0 = Mayfair
+         * 1 = Akina
+         * 2 = Stortford Lodge
+         * 3 = Mahora */
+        private string[][] neighbourSuburbs = new string[4][]
+        {
+            new string[] {"Akina", "Mahora", "Outside City"},
+            new string[] {"Mayfair", "Stortford Lodge", "Outside City"},
+            new string[] {"Akina", "Mahora", "Outside City"},
+            new string[] {"Stortford Lodge", "Mayfair", "Outside City"}
+        };
+
+        private string[][] streets = new string[4][]
+        {
+            new string[] {"Willowpark Road", "Riverslea Road"},
+            new string[] {},
+            new string[] {},
+            new string[] {}
+        };
+
+
+        //Generates a random number from a provided seed
+        int randomNumberGenerator()
         {
             int randomNumber = 0;
             return randomNumber;
         }
 
+        
         void drive()
         {
             foreach(int driver in drivers)
