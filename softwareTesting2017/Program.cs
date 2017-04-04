15b17b64b61a95bc51526a0faee40b8baca16ccc
Jack <jack_waayer@hotmail.co.nz>
2017-04-04 21:31:58 +1200

    Created a Simulation class and populated it with some basic arrays, variables and methods.

--- softwareTesting2017/Program.cs
+++ softwareTesting2017/Program.cs
@@ -10,7 +10,31 @@ namespace softwareTesting2017
     {
         static void Main(string[] args)
         {
-            
+            Simulation mySim = new Simulation();
         }
+    }
+
+    class Simulation
+    {
+        private string[] locations = new string[5] {"Mayfair", "Akina", "Stortford Lodge", "Mahora", "Outside City"};
+        private string[] exits = new string[4] { "Karamu Road", "Havelock Road", "Railway Road", "Omahu Road" };
+        private string[] streets;
+        private int[] drivers = new int[5] { 1, 2, 3, 4, 5 };
+        private int akinaCount = 0;
+
+        int randomNumberGenerator(int seed)
+        {
+            int randomNumber = 0;
+            return randomNumber;
+        }
+
+        void drive()
+        {
+            foreach(int driver in drivers)
+            {
+
+            }
+        }
+
     }
 }
